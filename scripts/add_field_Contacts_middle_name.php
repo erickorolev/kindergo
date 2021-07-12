a59b028734a6ba06eeba1b4fadc5e67262fc544b
Eric Korolev <erickorolev@gmail.com>
2021-07-12 14:25:34 +0500

    Contacts - Новые поля

--- scripts/add_field_Contacts_middle_name.php
+++ scripts/add_field_Contacts_middle_name.php
@@ -21,13 +21,13 @@ $module = Vtiger_Module::getInstance('Contacts'); // Имя модуля из т
 if ($module) {
     $block = Vtiger_Block::getInstance('LBL_CONTACT_INFORMATION', $module); // Название блока из таблицы vtiger_blocks
     if ($block) {
-        $field = Vtiger_Field::getInstance('metro_station', $module); // Название поля без пробелов через нижнее подчеркивание
+        $field = Vtiger_Field::getInstance('middle_name', $module); // Название поля без пробелов через нижнее подчеркивание
         if (!$field) {
             $field               = new Vtiger_Field();
-            $field->name         = 'metro_station';  // Название поля без пробелов через нижнее подчеркивание
+            $field->name         = 'middle_name';  // Название поля без пробелов через нижнее подчеркивание
             $field->table        = $module->basetable;
-            $field->label        = 'LBL_METRO_STATION';  // Лейбл на английском. Переводить на русский через файлы-переводов.
-            $field->column       = 'metro_station';  // Название поля без пробелов через нижнее подчеркивание
+            $field->label        = 'LBL_MIDDLE_NAME';  // Лейбл на английском. Переводить на русский через файлы-переводов.
+            $field->column       = 'middle_name';  // Название поля без пробелов через нижнее подчеркивание
             $field->columntype   = 'VARCHAR(255)';  // Посмотреть тип у похожих полей в таблице vtiger_навание-модуля
             
             // Посмотреть тип у похожих полей в таблице vtiger_field 
